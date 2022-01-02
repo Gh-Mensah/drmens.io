e9760736a539f6fd40f55776f8d28b1c2df0a970
Gh-Mensah <mkwaku055@gmail.com>
2022-01-03 03:31:41 +0800

    Adding react js#2

---
+++ test.js
@@ -0,0 +1,11 @@
+class App extends React.component{
+    render(){
+        return(
+            <div>
+                <p>Kofi is a boy</p>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<App/>,document.getElementById("app"));
