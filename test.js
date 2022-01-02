b684841655d954ac63a1ddc838ca1f2ea5a6a5d6
Gh-Mensah <mkwaku055@gmail.com>
2022-01-03 02:41:42 +0800

    test react.js typing

--- test.js
+++ test.js
@@ -1,9 +1,28 @@
-class App extends React.Component{
-    render(){
-        return(
-            <div><h1>HEllo guys</h1>
-            </div>
-        )
-    }
+import React from 'react';
+//importing typewriter-effect
+import Typewriter from "typewriter-effect";
+
+function App() {
+return (
+	<div id="fmapp">
+	<Typewriter
+
+	onInit={(typewriter)=> {
+
+	typewriter
+		
+	.typeString("GeeksForGeeks")
+		
+	.pauseFor(1000)
+	.deleteAll()
+	.typeString("Welcomes You")
+	.start();
+	}}
+	/>
+	</div>
+);
 }
-ReactDOM.render(<App/>,document.getElementById("app"));
+ReactDOM.render(
+    <App/>, 
+    document.getElementById("app")
+);
