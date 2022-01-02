905772aff48e37a3e41cc8e389b7ed369d08b07f
Gh-Mensah <mkwaku055@gmail.com>
2022-01-02 20:16:02 +0800

    Adding React to website #1

---
+++ test.js
@@ -0,0 +1,9 @@
+class App extends React.Component{
+    render(){
+        return(
+            <div><h1>HEllo guys</h1>
+            </div>
+        )
+    }
+}
+ReactDOM.render(<App/>,document.getElementById("app"));
