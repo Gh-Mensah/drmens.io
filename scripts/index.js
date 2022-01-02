a94d3b001478f6272250cc13bd3fc036248377ee
Gh-Mensah <mkwaku055@gmail.com>
2022-01-03 07:15:27 +0800

    correction react

--- scripts/index.js
+++ scripts/index.js
@@ -2,4 +2,4 @@ import React from "react";
 import ReactDOM  from "react-dom";
 import Move from "./Move";
 
-ReactDOM,render(<Move/>,document.getElementById("app"));
+ReactDOM.render(<Move/>,document.getElementById("app"));
