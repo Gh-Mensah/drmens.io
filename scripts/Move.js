01154477ab02b620d1c9c7baab978e129ccaf142
Gh-Mensah <mkwaku055@gmail.com>
2022-01-03 07:02:03 +0800

    Adding react

---
+++ scripts/Move.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class Move extends  Component{
+    render(){
+        return(
+            <div id="app">
+            <p>Kofi is a boy</p>
+            </div>
+        );
+    }
+}
+export default Move;
---
+++ scripts/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM  from "react-dom";
+import Move from "./Move";
+
+ReactDOM,render(<Move/>,document.getElementById("app"));
