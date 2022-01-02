1498d6f2817e26675deb0a9f6ef2ef9570f7e7e9
Gh-Mensah <mkwaku055@gmail.com>
2022-01-03 04:40:36 +0800

    react #6

--- test.js
+++ test.js
@@ -1,10 +1,27 @@
-class App extends React.Component{
-    render(){
-        return(
-            <div id="app">
-                <p>Kofi is a boy</p>
-            </div>
-        )
+import React from 'react';
+
+//importing typewriter-effect
+import Typewriter from "typewriter-effect";
+
+function App() {
+return (
+	<div className="App">
+	<Typewriter
+
+	onInit={(typewriter)=> {
+
+	typewriter
+		
+	.typeString("GeeksForGeeks")
+		
+	.pauseFor(1000)
+	.deleteAll()
+	.typeString("Welcomes You")
+	.start();
+	}}
+	/>
+	</div>
+);
 }
-}
-ReactDOM.render(<App/>,document.getElementById("app"));
+
+export default App;
