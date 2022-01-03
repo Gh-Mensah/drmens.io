decb70cd5cc959ad3ea4a8965beaf677ab34b8a3
Gh-Mensah <mkwaku055@gmail.com>
2022-01-03 19:20:22 +0800

    Finalizing

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-var quoteArray = ["I am Kwaku Asare,a <br>full MERN Stack developer."];
-var textPosition = 0; 
+const quoteArray = ["I am Kwaku Asare,a<br/>full MERN Stack developer."];
+let textPosition = 0; 
 // speed in milliseconds
-var speed = 150;
+const speed = 150;
 typewriter = () => {
   document.querySelector(".dtextinfo").innerHTML = `${quoteArray[0].substring(0, textPosition)}<code>|</code>`;
   
