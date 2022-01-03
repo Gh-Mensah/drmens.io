6fa8e4ff9b7a94b50ba4c1c0abe8d1c622f8494d
Gh-Mensah <mkwaku055@gmail.com>
2022-01-03 17:55:31 +0800

    #9

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,17 @@
-class Car extends React.Component {
-    render() {
-      return (
-          <div id="camp">
-      <h2>Hi, I am a Car!</h2>
-      </div>
-        );
-    }
-  }
-  
-  ReactDOM.render(<Car />, document.getElementById('app'));
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function Car(props) {
+  return (<h2>I am a { props.brand }!</h2>);
+}
+
+function Garage() {
+  return (
+    <>
+	    <h1>Who lives in my garage?</h1>
+	    <Car brand="Ford" />
+    </>
+  );
+}
+
+ReactDOM.render(<Garage />, document.getElementById('app'));
