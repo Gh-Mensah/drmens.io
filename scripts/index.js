9228a5637d9c569cf3c54946f237bac165259774
Gh-Mensah <mkwaku055@gmail.com>
2022-01-03 17:06:49 +0800

    #6

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "https://unpkg.com/react@17/umd/react.development.js";
+import ReactDOM from "https://unpkg.com/react-dom@17/umd/react-dom.development.js";
 
-function Car(props) {
-  return <h2>I am a { props.brand }!</h2>;
+function Car(props){
+  return <h4>I am a {props.brand}!</h4>;
 }
 
-function Garage() {
+function Garage(){
   return (
     <>
-	    <h1>Who lives in my garage?</h1>
-	    <Car brand="Ford" />
+	    <h5>Who lives in my garage?</h5>
+	    <Car brand="Ford"/>
     </>
   );
 }
 
-ReactDOM.render(<Garage />, document.getElementById('app'));
+ReactDOM.render(<Garage/>, document.getElementById('app'));
