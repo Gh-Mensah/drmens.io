dac91f6f821006fca712fd0948f17ef7e8baad64
Gh-Mensah <mkwaku055@gmail.com>
2022-01-03 16:47:17 +0800

    updating react code

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,17 @@
-class App extends React.Component{
-    render(){
-        return(
-            <div id="apps">
-            <p>Kofi is a boy</p>
-            </div>
-        )
-    }
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function Car(props) {
+  return <h2>I am a { props.brand }!</h2>;
 }
-ReactDOM.render(<App/>,document.getElementById("app"));
+
+function Garage() {
+  return (
+    <>
+	    <h1>Who lives in my garage?</h1>
+	    <Car brand="Ford" />
+    </>
+  );
+}
+
+ReactDOM.render(<Garage />, document.getElementById('app'));
