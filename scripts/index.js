2642ca1fe8bdc29c49a5f0cdf5056e5243594660
Gh-Mensah <mkwaku055@gmail.com>
2022-01-03 18:47:12 +0800

    Recreating the typewriter effect

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,9 @@
-var quoteArray = ["Welcome to my website brothers!"];
+var quoteArray = ["I am Kwaku Asare,a <br>full MERN Stack developer."];
 var textPosition = 0; 
 // speed in milliseconds
-var speed = 100;
+var speed = 150;
 typewriter = () => {
-  document.querySelector("#app").innerHTML = `${quoteArray[0].substring(0, textPosition)}<span id="type">\u25AE</span>`;
+  document.querySelector(".dtextinfo").innerHTML = `${quoteArray[0].substring(0, textPosition)}<code>|</code>`;
   
   if(textPosition++ != quoteArray[0].length)
     setTimeout(typewriter, speed);
