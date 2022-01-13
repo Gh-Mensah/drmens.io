906fae802231eeb5a34a1763e5cd436183810c9a
Gh-Mensah <mkwaku055@gmail.com>
2022-01-13 15:26:56 +0000

    ADDING JS to the Info Tab

---
+++ scripts/aboutscript.js
@@ -0,0 +1,13 @@
+function openInfo(evt,infoText) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("nav-link");
+    for (i = 0; i < tabcontent.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(infoText).style.display = "block";
+    evt.currentTarget.className += " active";
+}
